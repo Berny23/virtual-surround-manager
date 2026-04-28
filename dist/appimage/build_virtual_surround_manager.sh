cef65601c8c3b3d80d04957cb93aec1a638eb173
Berny23 <36038743+Berny23@users.noreply.github.com>
2026-04-29 00:50:40 +0200

    separate dependencies from appimage script

--- dist/appimage/build_virtual_surround_manager.sh
+++ dist/appimage/build_virtual_surround_manager.sh
@@ -1,49 +1,27 @@
+#!/bin/sh
 
 set -eux
 
 ARCH="$(uname -m)"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
-export ICON=/usr/share/icons/hicolor/scalable/apps/de.berny23.virtual_surround_manager.svg
-export DESKTOP=/usr/share/applications/de.berny23.virtual_surround_manager.desktop
-export OUTPATH=/dist
+export ICON=./build_install/share/icons/hicolor/scalable/apps/de.berny23.virtual_surround_manager.svg
+export DESKTOP=./build_install/share/applications/de.berny23.virtual_surround_manager.desktop
+export OUTPATH=./build_appimage
 export MAIN_BIN=virtual-surround-manager
 export OUTNAME=virtual-surround-manager-"$ARCH".AppImage
-
-pacman -Syu --noconfirm \
-	base-devel       \
-	curl             \
-	git              \
-	wget             \
-	xorg-server-xvfb \
-	zsync \
-	ninja \
-	libpipewire \
-	base-devel \
-	extra-cmake-modules \
-	cmake \
-	kirigami \
-	ki18n \
-	kcoreaddons \
-	breeze \
-	kiconthemes \
-	qt6-base \
-	qt6-declarative \
-	qqc2-desktop-style
 
 echo "Building Application"
 echo "---------------------------------------------------------------"
 
-git clone https://github.com/Berny23/virtual-surround-manager.git
-cd virtual-surround-manager
-cmake -B build -G Ninja
+cmake -B build -G Ninja -DIS_APPIMAGE=1 --install-prefix $(pwd)/build_install
 cmake --build build --config Release
 cmake --install build --config Release
 
 echo "Bundling AppImage..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun /usr/bin/virtual-surround-manager
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun.sh
+chmod +x ./quick-sharun.sh
+./quick-sharun.sh ./build_install/bin/virtual-surround-manager
 
-./quick-sharun --make-appimage
+./quick-sharun.sh --make-appimage
