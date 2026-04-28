a8f999a0bdba4d3a5e5781d3b7191dc762e8fd6d
Berny23 <Berny23@posteo.de>
2026-04-29 01:30:43 +0200

    fix broken hidden unicode character in GH action

--- dist/appimage/build_virtual_surround_manager.sh
+++ dist/appimage/build_virtual_surround_manager.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-ARCH=$(/usr/bin/uname -m)
+ARCH=$(uname -m)
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
 export ARCH
@@ -15,14 +15,14 @@ export OUTNAME=virtual-surround-manager-"$ARCH".AppImage
 echo "Building Application"
 echo "---------------------------------------------------------------"
 
-/usr/bin/cmake -B build -G Ninja -DIS_APPIMAGE=1 --install-prefix $(pwd)/build_install
-/usr/bin/cmake --build build --config Release
-/usr/bin/cmake --install build --config Release
+cmake -B build -G Ninja -DIS_APPIMAGE=1 --install-prefix $(pwd)/build_install
+cmake --build build --config Release
+cmake --install build --config Release
 
 echo "Bundling AppImage..."
 echo "---------------------------------------------------------------"
-/usr/bin/wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun.sh
-/usr/bin/chmod +x ./quick-sharun.sh
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun.sh
+chmod +x ./quick-sharun.sh
 ./quick-sharun.sh ./build_install/bin/virtual-surround-manager
 
 ./quick-sharun.sh --make-appimage
