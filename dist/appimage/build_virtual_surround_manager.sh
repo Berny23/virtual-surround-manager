0cf719c42f47725cb85b57a56c458797d765e17a
Berny23 <Berny23@posteo.de>
2026-04-29 01:10:48 +0200

    try fixing GH action

--- dist/appimage/build_virtual_surround_manager.sh
+++ dist/appimage/build_virtual_surround_manager.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
+ARCH=$(uname -m)
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
+export ARCH
 export ICON=./build_install/share/icons/hicolor/scalable/apps/de.berny23.virtual_surround_manager.svg
 export DESKTOP=./build_install/share/applications/de.berny23.virtual_surround_manager.desktop
 export OUTPATH=./build_appimage
